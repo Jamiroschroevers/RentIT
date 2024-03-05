f15b3af30aa9e734902d18d4007a5d4c922e0684
Jamiroschroevers <D287404@edu.curio.nl>
2024-03-05 12:53:54 +0100

    RENT-23 Foto's kunnen maken voor en na het onderhoud

--- app/Http/Controllers/MalfunctionHandlingController.php
+++ app/Http/Controllers/MalfunctionHandlingController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Image;
 use App\Models\Malfunction;
 use App\Models\MalfunctionHandling;
 use Illuminate\Http\Request;
@@ -16,21 +17,35 @@ class MalfunctionHandlingController extends Controller
     public function store(Request $request)
     {
         request()->validate([
-            'activities'       => ['required'],
+            'activities'  => ['required'],
             'description' => ['required'],
-            'mileage'       => ['required'],
-            'material' => ['required'],
+            'mileage'     => ['required'],
+            'material'    => ['required'],
             'Hoeveelheid' => ['required'],
-            'Kosten' => ['required']
+            'Kosten'      => ['required'],
         ]);
 
-        $storing  = new MalfunctionHandling();
-        $storing->activities       = $request->activities;
+        $storing              = new MalfunctionHandling();
+        $storing->activities  = $request->activities;
         $storing->description = $request->description;
-        $storing->material = $request->material;
-        $storing->mileage = $request->mileage;
-        $storing->cost = $request->Hoeveelheid * $request->Kosten;
-        $storing->save();
+        $storing->material    = $request->material;
+        $storing->mileage     = $request->mileage;
+        $storing->cost        = $request->Hoeveelheid * $request->Kosten;
+        $images               = request()->image;
+
+        if (isset($images)) {
+            $storing->save();
+        }
+        foreach ($images as $image) {
+            $imageNewFileName = time() . random_int(1, 99) . '.' . $image->extension();
+            $image->storeAs('public', $imageNewFileName);
+
+            $image        = new Image();
+            $image->name  = $imageNewFileName;
+            $image->url  = 'storage/';
+            $image->MH_id = $storing->id;
+            $image->save();
+        }
 
         return redirect()->route('dashboard');
     }
